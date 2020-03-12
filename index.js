d4f4365b85535d31ac16bd72b2ac4f5599cb65d8
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-12 12:06:40 -0400

    first commit

--- index.js
+++ index.js
@@ -3,4 +3,25 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+
+  // move function
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      move('left') // move left
+    }
+    if (e.key === "ArrowRight") {
+      move('right') // move right
+    }
+    if (e.key === "ArrowUp") {
+      move('up') // move up
+    }
+    if (e.key === "ArrowDown") {
+      move('down') // move down
+    }
+  });
+  
+  
+  
+  
+  
 })
