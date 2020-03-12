acfecab6d259d8b162403c290d7cbb55e9cfa650
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-12 13:00:38 -0400

    move the robot every second

--- index.js
+++ index.js
@@ -5,23 +5,51 @@ document.addEventListener("DOMContentLoaded", function(){
   // ADD CODE HERE!
 
   // move function
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      move('left') // move left
-    }
-    if (e.key === "ArrowRight") {
-      move('right') // move right
-    }
-    if (e.key === "ArrowUp") {
-      move('up') // move up
-    }
-    if (e.key === "ArrowDown") {
-      move('down') // move down
-    }
+  let start = document.getElementById("move-button")
+
+  start.addEventListener("click", function(e) {
+    console.log('start')
+    // let yPosition = parseInt(document.getElementById('robot').attributes[2].value)
+    
+    setInterval(function() {
+        
+        move('down')
+      }, 1000
+    )
+    
+    window.addEventListener("keydown", function(e) {
+        if (e.key === "ArrowLeft") {
+          move('left') // move left
+        }
+        if (e.key === "ArrowRight") {
+          move('right') // move right
+        }
+        if (e.key === "ArrowUp") {
+          move('up') // move up
+        }
+        if (e.key === "ArrowDown") {
+          move('down') // move down
+        }
+      });
   });
-  
-  
-  
-  
-  
+
+
 })
+
+
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     move('left') // move left
+//   }
+//   if (e.key === "ArrowRight") {
+//     move('right') // move right
+//   }
+//   if (e.key === "ArrowUp") {
+//     move('up') // move up
+//   }
+//   if (e.key === "ArrowDown") {
+//     move('down') // move down
+//   }
+// });
+
+
